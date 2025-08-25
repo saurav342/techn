3f214f11a05d1f078414adb95d53939971f89ed4
Saurav <sauravks2010@gmail.com>
2025-08-25 16:07:13 +0530

    added

--- src-index.js
+++ src-index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import TechiniumLanding from './TechiniumLanding';
+import TechiniumLanding from './src/TechiniumLanding';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
