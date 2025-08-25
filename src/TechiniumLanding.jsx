bf343868ed3d095ca14353224f581bf7d93726d4
Saurav <sauravks2010@gmail.com>
2025-08-25 16:05:40 +0530

    add

--- src/TechiniumLanding.jsx
+++ src/TechiniumLanding.jsx
@@ -1,12 +1,1 @@
-import React from "react";
-
-export default function TechiniumLanding() {
-  return (
-    <div style={{ padding: 24 }}>
-      <h1>Techinium</h1>
-      <p>Build successful. Replace this placeholder with the landing component content.</p>
-    </div>
-  );
-}
-
-
+export { default } from "../TechiniumLanding.jsx";
